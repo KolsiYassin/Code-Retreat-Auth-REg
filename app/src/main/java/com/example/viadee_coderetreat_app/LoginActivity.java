af6f218f3ad07c8010fbcfe13b928a1aaa7ac4de
KolsiYassin <kolsiyassin772@gmail.com>
2025-06-06 21:40:53 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/viadee_coderetreat_app/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.viadee_coderetreat_app;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // completely immersive (no time and battery, just for login and reg, i find it better)
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        //Get TextViews and Buttons
+        TextView emailTextView = findViewById(R.id.name_edit_text);
+        TextView passwordTextView = findViewById(R.id.password_edit_text);
+        Button login = findViewById(R.id.Login);
+        Button register = findViewById(R.id.register);
+
+        // Slide up logo
+        ImageView logo = findViewById(R.id.logoImage);
+        logo.animate()
+                .translationYBy(-600f)
+                .setDuration(1500)
+                .withEndAction(() -> {
+                    // Animation done: show text fields and buttons
+                    emailTextView.setVisibility(View.VISIBLE);
+                    passwordTextView.setVisibility(View.VISIBLE);
+                    login.setVisibility(View.VISIBLE);
+                    register.setVisibility(View.VISIBLE);
+                })
+                .start();
+    }
+}
